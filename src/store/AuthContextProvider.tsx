2a611fcffb2b9bc03c532b589a41a334a30abd5f
Oleksandr <tokyopac@gmail.com>
2026-05-21 10:16:07 +0300

    feat: add prettier for code formatting and clean up AuthContextProvider

--- src/store/AuthContextProvider.tsx
+++ src/store/AuthContextProvider.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { instance } from '../api/axiosInstance';
-import { AuthContext } from './AuthContext';
-import type { AuthContextType, UserData, User } from '../types/AuthContext';
+import React, { useEffect, useMemo, useState } from "react";
+import { instance } from "../api/axiosInstance";
+import { AuthContext } from "./AuthContext";
+import type { AuthContextType, UserData, User } from "../types/AuthContext";
 
 export type Props = {
   children: React.ReactNode;
 };
 
 export const AuthContextProvider: React.FC<Props> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user] = useState<User | null>(null);
   const [accessToken, setAccessToken] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading] = useState(true);
 
   const isAuthenticated = Boolean(user && accessToken);
 
   const register = async (data: UserData) => {
-    const response = await instance.post('/register', data);
+    const response = await instance.post("/register", data);
 
     console.log(response);
 
@@ -23,36 +23,36 @@ export const AuthContextProvider: React.FC<Props> = ({ children }) => {
   };
 
   const login = async (data: UserData) => {
-    const response = await instance.post('/login', data);
+    const response = await instance.post("/login", data);
 
     setAccessToken(response.data.access_token);
   };
 
   const logout = async () => {
     try {
-      await instance.post('/logout');
+      await instance.post("/logout");
     } finally {
       setAccessToken(null);
     }
   };
 
-  useEffect(() => {
-    const refreshSession = async () => {
-      try {
-        const response = await instance.post('/refresh');
+  // useEffect(() => {
+  //   const refreshSession = async () => {
+  //     try {
+  //       const response = await instance.post('/refresh');
 
-        setUser(response.data.user);
-        setAccessToken(response.data.accessToken);
-      } catch {
-        setUser(null);
-        setAccessToken(null);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  //       setUser(response.data.user);
+  //       setAccessToken(response.data.accessToken);
+  //     } catch {
+  //       setUser(null);
+  //       setAccessToken(null);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
 
-    // refreshSession();
-  }, []);
+  //   refreshSession();
+  // }, []);
 
   useEffect(() => {
     const interceptorId = instance.interceptors.request.use((config) => {
