922b421f9bb2db296a3502c6f433962c79703770
Oleksandr <tokyopac@gmail.com>
2026-05-22 19:55:23 +0300

    feat: remove unused beer API module

--- src/api/beers.ts
+++
@@ -1,6 +0,0 @@
-import type { BeerResponse } from '../types/Beer';
-import { client } from '../utils/axiosClient';
-
-export const getBeer = () => {
-  return client.get<BeerResponse>('/beers');
-};
