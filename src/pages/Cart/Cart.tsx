d14621a6341e2ef9b64f1d1bdb11b5d0eef58164
Oleksandr <tokyopac@gmail.com>
2026-06-12 19:39:59 +0300

    feat: start implementing Cart component layout with main and summary sections

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,22 @@
 import styles from './Cart.module.scss';
 
 export const Cart = () => {
-  return <section className={styles.cart}></section>;
+  return (
+    <section className={styles.cart}>
+      <div className={styles.cartMain}>
+        <div className={styles.cartMainText}>
+          <h1 className={styles.cartMainTitle}>Cart</h1>
+          <button className={styles.cartMainClearCart}>Clear Cart</button>
+        </div>
+
+        <div className={styles.cartMainProductList}></div>
+      </div>
+
+      <div className={styles.cartSummary}>
+        <h2 className={styles.cartSummaryTitle}>Your summary</h2>
+
+        <div className={styles.cartSummaryTotal}></div>
+      </div>
+    </section>
+  );
 };
