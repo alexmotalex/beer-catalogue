13b7756584ff6f9170e7682f0a9390e400e6935f
Alex <tokyopac@gmail.com>
2026-06-21 20:57:12 +0300

    feat: add route exclusion for scrollToTop functionality to prevent scrolling on cart page

--- src/utils/scrollToTop.ts
+++ src/utils/scrollToTop.ts
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
+import { ROUTES } from '../constants/routes';
 
 export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    const excludedRoutes = [ROUTES.cart];
+
+    console.log(excludedRoutes, pathname);
+
+    if (excludedRoutes.some(route => route === pathname)) {
+      return;
+    }
+
     window.scrollTo(0, 0);
   }, [pathname]);
 
