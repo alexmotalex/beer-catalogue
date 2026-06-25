c503ad16f69a05505befdf8404c368adf2de72c5
Alex <tokyopac@gmail.com>
2026-06-25 15:07:04 +0300

    feat: update error icon in SVG and remove unused close shape SVG
    - Updated the error icon SVG path for improved design.
    - Removed the unused close shape SVG file.
    - Changed router from HashRouter to BrowserRouter in index.tsx for better routing.
    - Set the base path in vite.config.ts for deployment.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -9,7 +9,7 @@ import { ScrollToTop } from './utils/scrollToTop.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <Router basename="/beer-catalogue/">
       <ScrollToTop />
       <AuthContextProvider>
         <BeerContextProvider>
