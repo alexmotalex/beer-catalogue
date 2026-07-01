4d24a16ef82f5e350ea66416eac053907e56055b
Oleksandr <tokyopac@gmail.com>
2026-07-01 19:48:10 +0300

    fix: update server wake-up time message from 40 seconds to one minute

--- src/components/SlowServerMessage/SlowServerMessage.tsx
+++ src/components/SlowServerMessage/SlowServerMessage.tsx
@@ -6,8 +6,8 @@ export const SlowServerMessage = () => (
     <Spinner />
     <p className={styles.title}>Waking up the server...</p>
     <p className={styles.subtitle}>
-      Our server starts from sleep on first visit. This usually takes up to 40
-      seconds. Thank you for your patience!
+      Our server starts from sleep on first visit. This usually takes up to one
+      minute. Thank you for your patience!
     </p>
   </div>
 );
